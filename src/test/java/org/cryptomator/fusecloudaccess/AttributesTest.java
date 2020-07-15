6f7ba98f3667bdd676bfe145002431b613824179
Armin Schrenk <armin.schrenk@skymatic.de>
2020-07-15 12:28:03 +0200

    clean up

--- src/test/java/org/cryptomator/fusecloudaccess/AttributesTest.java
+++ src/test/java/org/cryptomator/fusecloudaccess/AttributesTest.java
@@ -5,14 +5,11 @@ import org.cryptomator.cloudaccess.api.CloudItemMetadata;
 import org.cryptomator.cloudaccess.api.CloudItemType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mockito;
 import ru.serce.jnrfuse.struct.FileStat;
 
 import java.nio.file.Path;
@@ -29,18 +26,18 @@ public class AttributesTest {
 	private FileStat fileStat;
 
 	@BeforeEach
-	public void setup(){
+	public void setup() {
 		fileStat = new FileStat(RUNTIME);
 	}
 
 	@ParameterizedTest
 	@MethodSource("provideMetadataOfDifferentTypes")
-	public void testfileTypesAreCorrect(CloudItemType type, int expectedBitsSet){
-		Attributes.copy(CloudItemMetadataProvider.ofType(type),fileStat);
-		Assertions.assertEquals(expectedBitsSet, fileStat.st_mode.intValue() & expectedBitsSet );
+	public void testfileTypesAreCorrect(CloudItemType type, int expectedBitsSet) {
+		Attributes.copy(CloudItemMetadataProvider.ofType(type), fileStat);
+		Assertions.assertEquals(expectedBitsSet, fileStat.st_mode.intValue() & expectedBitsSet);
 	}
 
-	private static Stream<Arguments> provideMetadataOfDifferentTypes(){
+	private static Stream<Arguments> provideMetadataOfDifferentTypes() {
 		return Stream.of(
 				Arguments.of(CloudItemType.FILE, FileStat.S_IFREG),
 				Arguments.of(CloudItemType.FOLDER, FileStat.S_IFDIR),
@@ -50,15 +47,15 @@ public class AttributesTest {
 
 	@ParameterizedTest
 	@MethodSource("provideInstantOrNull")
-	public void testMTimeSetCorrectly(Instant i){
-		var expectedInstant = Objects.isNull(i)?Instant.EPOCH : i;
-		Attributes.copy(CloudItemMetadataProvider.ofMTime(i),fileStat);
+	public void testMTimeSetCorrectly(Instant i) {
+		var expectedInstant = Objects.isNull(i) ? Instant.EPOCH : i;
+		Attributes.copy(CloudItemMetadataProvider.ofMTime(i), fileStat);
 
 		Assertions.assertEquals(expectedInstant.getEpochSecond(), fileStat.st_mtim.tv_sec.longValue());
 		Assertions.assertEquals(expectedInstant.getNano(), fileStat.st_mtim.tv_nsec.longValue());
 	}
 
-	private static Stream<Arguments> provideInstantOrNull(){
+	private static Stream<Arguments> provideInstantOrNull() {
 		return Stream.of(
 				Arguments.of(Instant.now()),
 				Arguments.of((Instant) null)
@@ -68,9 +65,9 @@ public class AttributesTest {
 	@ParameterizedTest
 	@NullSource
 	@ValueSource(longs = {123456L})
-	public void testSizeSetCorrectly(Long size){
-		final long expectedSize = Objects.isNull(size)? 0 : size;
-		Attributes.copy(CloudItemMetadataProvider.ofSize(size),fileStat);
+	public void testSizeSetCorrectly(Long size) {
+		final long expectedSize = Objects.isNull(size) ? 0 : size;
+		Attributes.copy(CloudItemMetadataProvider.ofSize(size), fileStat);
 		Assertions.assertEquals(expectedSize, fileStat.st_size.longValue());
 
 	}
@@ -84,28 +81,28 @@ public class AttributesTest {
 		private static final Instant DEFAULT_MTIME = Instant.now();
 		private static final long DEFAULT_SIZE = 1337L;
 
-		public static CloudItemMetadata of(String name, Path p, CloudItemType type, Instant mTime ,Long size){
+		public static CloudItemMetadata of(String name, Path p, CloudItemType type, Instant mTime, Long size) {
 			return new CloudItemMetadata(name, p, type, Optional.ofNullable(mTime), Optional.ofNullable(size));
 		}
 
-		public static CloudItemMetadata ofName(String name){
-			return CloudItemMetadataProvider.of(name, DEFAULT_PATH, DEFAULT_TYPE,DEFAULT_MTIME,DEFAULT_SIZE);
+		public static CloudItemMetadata ofName(String name) {
+			return CloudItemMetadataProvider.of(name, DEFAULT_PATH, DEFAULT_TYPE, DEFAULT_MTIME, DEFAULT_SIZE);
 		}
 
-		public static CloudItemMetadata ofPath(Path p){
-			return CloudItemMetadataProvider.of(DEFAULT_NAME, p, DEFAULT_TYPE,DEFAULT_MTIME,DEFAULT_SIZE);
+		public static CloudItemMetadata ofPath(Path p) {
+			return CloudItemMetadataProvider.of(DEFAULT_NAME, p, DEFAULT_TYPE, DEFAULT_MTIME, DEFAULT_SIZE);
 		}
 
-		public static CloudItemMetadata ofType(CloudItemType type){
-			return CloudItemMetadataProvider.of(DEFAULT_NAME, DEFAULT_PATH, type,DEFAULT_MTIME,DEFAULT_SIZE);
+		public static CloudItemMetadata ofType(CloudItemType type) {
+			return CloudItemMetadataProvider.of(DEFAULT_NAME, DEFAULT_PATH, type, DEFAULT_MTIME, DEFAULT_SIZE);
 		}
 
-		public static CloudItemMetadata ofMTime(Instant mTime){
-			return CloudItemMetadataProvider.of(DEFAULT_NAME, DEFAULT_PATH, DEFAULT_TYPE,mTime,DEFAULT_SIZE);
+		public static CloudItemMetadata ofMTime(Instant mTime) {
+			return CloudItemMetadataProvider.of(DEFAULT_NAME, DEFAULT_PATH, DEFAULT_TYPE, mTime, DEFAULT_SIZE);
 		}
 
-		public static CloudItemMetadata ofSize(Long size){
-			return CloudItemMetadataProvider.of(DEFAULT_NAME, DEFAULT_PATH, DEFAULT_TYPE,DEFAULT_MTIME,size);
+		public static CloudItemMetadata ofSize(Long size) {
+			return CloudItemMetadataProvider.of(DEFAULT_NAME, DEFAULT_PATH, DEFAULT_TYPE, DEFAULT_MTIME, size);
 		}
 	}
 
