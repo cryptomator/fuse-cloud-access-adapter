a6a1348598365c0159c8c2f3ab00a41888b19e75
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2020-08-17 15:05:07 +0200

    Copied LockManager from fuse-nio-adapter

---
+++ src/main/java/org/cryptomator/fusecloudaccess/locks/PathWLockImpl.java
@@ -0,0 +1,31 @@
+package org.cryptomator.fusecloudaccess.locks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.function.Function;
+
+class PathWLockImpl extends PathLockImpl {
+
+	private static final Logger LOG = LoggerFactory.getLogger(PathWLockImpl.class);
+
+	private PathWLockImpl(List<String> pathComponents, Optional<PathLock> parent, ReadWriteLock lock, Function<List<String>, ReadWriteLock> dataLockSupplier) {
+		super(pathComponents, parent, lock, dataLockSupplier);
+	}
+
+	public static PathLockImpl create(List<String> pathComponents, Optional<PathLock> parent, ReadWriteLock lock, Function<List<String>, ReadWriteLock> dataLockSupplier) {
+		lock.writeLock().lock();
+		LOG.trace("Acquired write path lock for '{}'", pathComponents);
+		return new PathWLockImpl(pathComponents, parent, lock, dataLockSupplier);
+	}
+
+	@Override
+	public void close() {
+		LOG.trace("Released write path lock for '{}'", pathComponents);
+		lock.writeLock().unlock();
+		super.close();
+	}
+}
