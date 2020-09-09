2c7fb4bb19d8215e4e3c4c8f13841b466dcc96d5
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2020-09-09 21:31:18 +0200

    Added support function for locked CompletableFutures

--- src/main/java/org/cryptomator/fusecloudaccess/CompletionUtils.java
+++ src/main/java/org/cryptomator/fusecloudaccess/CompletionUtils.java
@@ -2,9 +2,16 @@ package org.cryptomator.fusecloudaccess;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.locks.StampedLock;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 class CompletionUtils {
+
+	public static <T> CompletionStage<T> runLocked(StampedLock lock, Supplier<CompletionStage<T>> completionStage) {
+		long stamp = lock.writeLock();
+		return runAlways(completionStage.get(), () -> lock.unlock(stamp));
+	}
 
 	public static <T> CompletionStage<T> runAlways(CompletionStage<T> completionStage, Runnable runAlways) {
 		return completionStage.handle((result, exception) -> {
